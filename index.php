96ca53b14277e054d5e08897aa2564d272cef511
simonemoretti94 <simone.moretti.944@gmail.com>
2024-03-17 22:09:44 +0100

    8th styling

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
         <div class="row justify-content-evenly ">
             <?php foreach ($dogsProducts as $product) : ?>
 
-                <div id="wrapper-padding " class="col-xxl-3 col-lg-4 col-md-4 col-sm-6" style="padding: .5rem;">
+                <div id="wrapper-padding " class="col-xxl-3 col-lg-4 col-md-4 col-sm-6">
                     <div id="card" class="card ">
                         <div class="card-header">
                             <img class="card-img-top p-1" src="<?= $product->path ?>" alt="<?= $product->name ?>">
@@ -51,7 +51,9 @@
                         <div class="card-footer">
                             <div class="row justify-content-evenly ">
                                 <?php foreach ($product->categories as $category) : ?>
-                                    <p class="col-xxl-3 col-xl-3 col-md-5 col-sm-6"><?= $category ?></p>
+                                    <div id="wrapper-padding" class="col-xxl-4 col-xl-4 col-md-6 col-sm-6">
+                                        <p><?= $category ?></p>
+                                    </div>
                                 <?php endforeach; ?>
                             </div>
                         </div>
@@ -64,23 +66,26 @@
 
     </div>
 
-    <div class="container-fluid mt-2">
+    <div class="container-fluid my-3">
         <div class="row justify-content-evenly ">
             <?php foreach ($catsProducts as $product) : ?>
 
-                <div id="wrapper-padding " class="col-xxl-3 col-lg-4 col-md-4 col-sm-6" style="padding: .5rem;">
+                <div id="wrapper-padding " class="col-xxl-3 col-lg-4 col-md-4 col-sm-6">
                     <div id="card" class="card ">
                         <div class="card-header">
                             <img class="card-img-top p-1" src="<?= $product->path ?>" alt="<?= $product->name ?>">
                         </div>
                         <div class="card-body d-flex justify-content-between  ">
                             <h4 class="card-title col-lg-10 col-md-8 col-sm-8"><?= $product->name ?></h4>
+                            <h6 class="card-title col-lg-10 col-md-8 col-sm-8"><?= $product->name ?></h6>
                             <p class="card-text col-lg-2 col-md-4 col-sm-4 " style="text-align: right;"><?= $product->price ?>&euro;</p>
                         </div>
                         <div class="card-footer">
                             <div class="row justify-content-evenly ">
                                 <?php foreach ($product->categories as $category) : ?>
-                                    <p class="col-xxl-3 col-xl-3 col-md-5 col-sm-6"><?= $category ?></p>
+                                    <div id="wrapper-padding" class="col-xxl-4 col-xl-4 col-md-6 col-sm-6">
+                                        <p><?= $category ?></p>
+                                    </div>
                                 <?php endforeach; ?>
                             </div>
                         </div>
@@ -108,6 +113,11 @@
                 width: 100%;
                 height: 100%;
             }
+
+            >img:hover {
+                transform: scale(.99);
+                border-radius: 8px;
+            }
         }
 
         & .card-body {
@@ -118,18 +128,36 @@
             height: auto;
 
             >.row {
-                >p {
+                & p {
                     text-align: center;
                     border: solid 1px black;
                     border-radius: 8px;
 
                     font-size: small;
-                    vertical-align: middle;
+                    padding: 0;
+
+
                 }
             }
         }
     }
 
+    h4:hover,
+    h6:hover {
+        color: red;
+        text-decoration: underline;
+        font-weight: 500;
+    }
+
+    #wrapper-padding {
+        padding: .5rem;
+
+        >p:hover {
+            transform: scale(.9);
+            background-color: rgb(6, 220, 6);
+            color: white;
+        }
+    }
 
     @media screen and (min-width: 576px) {
         .card-body {
