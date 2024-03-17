31619039da30619394e000625ab72517c582c780
simonemoretti94 <simone.moretti.944@gmail.com>
2024-03-17 21:06:57 +0100

    4.3th

--- index.php
+++ index.php
@@ -10,51 +10,63 @@
         <?php var_dump($catsProducts) ?>
     </div>
 
-    <div class="col-xxl-3 col-lg-3 col-md-6 col-sm-12 d-none">
-        <p><?= $dogsProducts[0]->name ?></p>
-        <p><?= $dogsProducts[0]->price ?></p>
-        <img src="<?= $dogsProducts[0]->path ?>" alt="<?= $dogsProducts[0]->name ?>">
+    <div class="container-fluid mt-2 ">
         <div class="row">
-            <?php foreach ($dogsProducts[0]->categories as $category) : ?>
-                <p class="col-xxl-3 col-xl-3 col-md-4 col-sm-6"><?= $category ?></p>
-            <?php endforeach; ?>
-        </div>
 
-    </div>
-
-    <div id="card" class="card col-xxl-3 col-lg-3 col-md-6 col-sm-12">
-        <img class="card-img-top p-1" src="<?= $dogsProducts[0]->path ?>" alt="<?= $dogsProducts[0]->name ?>">
-        <div class="card-body d-flex ">
-            <h4 class="card-title col-10"><?= $dogsProducts[0]->name ?></h4>
-            <p class="card-text col-2"><?= $dogsProducts[0]->price ?>&euro;</p>
-        </div>
-        <div class="card-footer">
-            <div class="row justify-content-evenly ">
-                <?php foreach ($dogsProducts[0]->categories as $category) : ?>
-                    <p class="col-xxl-3 col-xl-3 col-md-4 col-sm-4"><?= $category ?></p>
-                <?php endforeach; ?>
+            <div id="card" class="card col-xxl-3 col-lg-3 col-md-4 col-sm-6">
+                <div class="card-header">
+                    <img class="card-img-top p-1" src="<?= $dogsProducts[0]->path ?>" alt="<?= $dogsProducts[0]->name ?>">
+                </div>
+                <div class="card-body d-flex ">
+                    <h4 class="card-title col-10"><?= $dogsProducts[0]->name ?></h4>
+                    <p class="card-text col-2"><?= $dogsProducts[0]->price ?>&euro;</p>
+                </div>
+                <div class="card-footer">
+                    <div class="row justify-content-evenly ">
+                        <?php foreach ($dogsProducts[0]->categories as $category) : ?>
+                            <p class="col-xxl-3 col-xl-3 col-md-5 col-sm-6"><?= $category ?></p>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
             </div>
+
         </div>
+
     </div>
 </main>
+
+
 <?php require_once __DIR__ . '/parts/footer.php' ?>
 
 <style>
     #card {
-        max-height: 300px;
-        max-width: 400px;
+        height: 450px;
 
-        & img {
-            width: 100%;
-            aspect-ratio: 1 / 1;
+        >.card-header {
+            height: 300px;
+
+            >img {
+                width: 100%;
+                height: 100%;
+            }
         }
-    }
 
-    .row {
-        >p {
-            text-align: center;
-            border: solid 1px black;
-            border-radius: 8px;
+        & .card-body {
+            height: 50px;
+        }
+
+        & .card-footer {
+            height: 100px;
+
+            >.row {
+                >p {
+                    text-align: center;
+                    border: solid 1px black;
+                    border-radius: 8px;
+
+                    font-size: medium;
+                }
+            }
         }
     }
 </style>
