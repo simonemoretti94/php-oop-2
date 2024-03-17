4ec8abf646c8722691165ec9a298d554ab72b2fb
simonemoretti94 <simone.moretti.944@gmail.com>
2024-03-17 20:44:36 +0100

    4.1th on card template

--- index.php
+++ index.php
@@ -5,7 +5,21 @@
 <main>
     <?php require_once __DIR__ . '/data/db.php'; ?>
 
-    <?php var_dump($dogsProducts) ?>
-    <?php var_dump($catsProducts) ?>
+    <div class="d-none">
+        <?php var_dump($dogsProducts) ?>
+        <?php var_dump($catsProducts) ?>
+    </div>
+
+    <div class="col-xxl-3 col-lg-3 col-md-6 col-sm-12">
+        <p><?= $dogsProducts[0]->name ?></p>
+        <p><?= $dogsProducts[0]->price ?></p>
+        <img src="<?= $dogsProducts[0]->path ?>" alt="<?= $dogsProducts[0]->name ?>">
+        <div class="row">
+            <?php foreach ($dogsProducts[0]->categories as $category) : ?>
+                <p class="col-xxl-3 col-xl-3 col-md-4 col-sm-6"><?= $category ?></p>
+            <?php endforeach; ?>
+        </div>
+
+    </div>
 </main>
 <?php require_once __DIR__ . '/parts/footer.php' ?>
